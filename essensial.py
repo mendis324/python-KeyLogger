2d06d1b612faa838caec75294caae8857c1b4632
mendis <menda9991@gmail.com>
2025-11-05 20:09:38 +0530

    project start

---
+++ essensial.py
@@ -0,0 +1,6 @@
+# storing the keystrokkes in the Text file
+# file handeling - how to read, write and append to thr file
+
+# r - reading
+# w - writing
+# a - appending 
---
+++ smartKeyLogger.py
@@ -0,0 +1,1 @@
+open("log.txt", )
